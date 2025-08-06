b0cbccde98057bdc81f84b5c30f7d3bbea54adf1
vishxlkr <vishxlkr@gmail.com>
2025-08-06 19:10:44 +0530

    update

---
+++ cses/Repetitions.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main(){
+
+    string s; 
+    cin>>s;
+
+    int i = 0 ; int j = 0;
+
+    while(j<s.size()){
+        
+    }
+
+    
+
+    return 0;
+}
