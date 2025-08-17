87d33d226dea1a5ae973c570ae2190abba294a56
vishxlkr <vishxlkr@gmail.com>
2025-08-17 15:27:28 +0530

    update

--- DP/tourist.cpp
+++ DP/tourist.cpp
@@ -4,15 +4,7 @@ using namespace std;
 
 #define ll long long int
 
-void file_i_o() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-// #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-// #endif
-}
+
 
 ll n, m;        // dimension of the input matrix
 char mat[105][105];
@@ -50,7 +42,7 @@ ll f(ll i, ll j, ll x ,ll y){
 
 int main(){
 
-    file_i_o();
+
 
     int t; 
     cin>>t;
