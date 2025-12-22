114f415a49e88acf5b70930249b7a9cf566f386e
vishxlkr <vishxlkr@gmail.com>
2025-12-22 23:07:57 +0530

    update

--- test2.cpp
+++ test2.cpp
@@ -11,7 +11,4 @@ int main(){
     vector<int> v = 
 
     return 0;
-}git quick
-
-git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
+}
